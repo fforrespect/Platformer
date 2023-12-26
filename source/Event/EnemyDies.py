5a62191f115ad2a64a27bc7898d0761504cbb425
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2023-12-26 12:41:55 +0400

    Slightly edited the enemy dying logic to make it more efficient

--- source/Event/EnemyDies.py
+++ source/Event/EnemyDies.py
@@ -3,9 +3,10 @@ from Object import Character
 from Setup import GlobalVars
 
 
-def process(enemy):
+def process(enemy, player_has_left_screen=False):
     Character.active_enemies.remove(enemy)
     GlobalVars.all_objects.remove(enemy)
 
-    Level.delete_enemy(enemy.enemy_hash)
+    if not player_has_left_screen:
+        Level.delete_enemy(enemy.enemy_hash)
 
