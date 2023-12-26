fce8f6cf79c4adcd0fe1375a0d5c425855306285
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2023-12-26 19:19:47 +0400

    -m grammar

--- source/Meta/Display/Window.py
+++ source/Meta/Display/Window.py
@@ -7,7 +7,7 @@ def display(screen):
     # Background
     screen.fill(Colours.BLACK)
 
-    # Iterate through the objects, and display them one by one
+    # Iterate through the objects, and draw them one by one
     for item in GlobalVars.all_objects + GlobalVars.all_overlays:
         item.draw(screen)
 
