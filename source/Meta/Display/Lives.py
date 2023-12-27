d3028955171ba701f9236bf80ba286b5dd14bd7b
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2023-12-27 10:57:54 +0400

    -m improved efficiency

--- source/Meta/Display/Lives.py
+++ source/Meta/Display/Lives.py
@@ -15,13 +15,12 @@ class Life:
     def __init__(self, number):
         self.x = Constants.LIVES_START_X - (number * Constants.LIVES_SPACING)
         self.y = Constants.LIVES_PADDING
-        self.image = f"{Constants.IMAGES_FP}life.png"
+
+        self.image = pygame.image.load(f"{Constants.IMAGES_FP}life.png")
+        self.image = pygame.transform.scale(self.image, (50, 50))
 
         GlobalVars.all_overlays.append(self)
 
 
     def draw(self, screen):
-        life_img = pygame.image.load(self.image)
-        life_img = pygame.transform.scale(life_img, (50, 50))
-
-        screen.blit(life_img, (self.x, self.y))
+        screen.blit(self.image, (self.x, self.y))
